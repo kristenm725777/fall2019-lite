38282a19665a9d4b6c656087164f24d5484102f9
Marina Kun <mk3qa@virginia.edu>
2019-10-19 19:08:48 -0400

    Add project 5

---
+++ week5/Week5Project/scripts.js
@@ -0,0 +1,7 @@
+function displayImage() {
+    document.getElementById("spoop").style.visibility = "visible";
+}
+
+function removeImage() {
+    document.getElementById("spoop").style.visibility = "hidden";
+}
